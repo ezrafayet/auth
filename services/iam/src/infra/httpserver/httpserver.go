e888809443efbfb8f2991d6b185ebf3ebcebb62f
adzaria <adzaria@protonmail.com>
2023-05-14 01:31:40 +0300

    added PORT to env

--- services/iam/src/infra/httpserver/httpserver.go
+++ services/iam/src/infra/httpserver/httpserver.go
@@ -23,34 +23,35 @@ func Start() error {
 	db := database.ConnectDB()
 	defer database.CloseDB(db)
 
-	reg := registry.NewRegistry(db)
+	r := registry.NewRegistry(db)
 
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
+	router := chi.NewRouter()
+
+	router.Use(middleware.Logger)
 
 	// User management endpoints
-	r.Get("/api/internal/v1/auth/whoami", nil)
-	r.Post("/api/internal/v1/auth/register", reg.UsersHandler.RegisterHandler)
+	router.Get("/api/internal/v1/auth/whoami", nil)
+	router.Post("/api/internal/v1/auth/register", r.UsersHandler.RegisterHandler)
 
 	// Email verification endpoints
-	r.Post("/api/internal/v1/auth/email-verification/send", nil)
-	r.Patch("/api/internal/v1/auth/email-verification/verify", nil)
+	router.Post("/api/internal/v1/auth/email-verification/send", nil)
+	router.Patch("/api/internal/v1/auth/email-verification/verify", nil)
 
 	// Authentication endpoints
-	r.Post("/api/internal/v1/auth/magic-link", nil)
-	r.Post("/api/internal/v1/auth/token", nil)
-	r.Post("/api/internal/v1/auth/token/authorize", nil)
-	r.Post("/api/internal/v1/auth/token/refresh", nil)
-	r.Post("/api/internal/v1/auth/token/revoke", nil)
+	router.Post("/api/internal/v1/auth/magic-link", nil)
+	router.Post("/api/internal/v1/auth/token", nil)
+	router.Post("/api/internal/v1/auth/token/authorize", nil)
+	router.Post("/api/internal/v1/auth/token/refresh", nil)
+	router.Post("/api/internal/v1/auth/token/revoke", nil)
 
 	// Other
-	r.Get("/api/internal/v1/auth/foobar", func(w http.ResponseWriter, r *http.Request) {
+	router.Get("/api/internal/v1/auth/foobar", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("headers", r.Header.Get("X-Request-Id"))
 		w.Write([]byte("{\"status\":\"success\"}"))
 	})
 
-	r.HandleFunc("/*", httphelpers.WriteError(http.StatusNotFound, "error", "NOT_FOUND"))
+	router.HandleFunc("/*", httphelpers.WriteError(http.StatusNotFound, "error", "NOT_FOUND"))
 
-	fmt.Println("Server started on port 7777")
-	return http.ListenAndServe(":7777", r)
+	fmt.Println("Server started on port " + os.Getenv("PORT"))
+	return http.ListenAndServe(":"+os.Getenv("PORT"), router)
 }
