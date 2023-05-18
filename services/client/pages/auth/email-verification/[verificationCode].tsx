c4fe9e1bb7ed4de838e9a7bbe280fcf4962e2bdc
adzaria <adzaria@protonmail.com>
2023-05-19 02:29:09 +0300

    added a route for the email verification

--- services/client/pages/auth/email-verification/[verificationCode].tsx
+++ services/client/pages/auth/email-verification/[verificationCode].tsx
@@ -1,3 +1,32 @@
+import {useEffect} from "react";
+import {useRouter} from "next/router";
+
 export default (props) => {
-    return <></>
+    const router = useRouter()
+
+    const { verificationCode } = router.query
+
+    useEffect(() => {
+        (async () => {
+            if (verificationCode) {
+                const unencodedVerificationCode = decodeUrlSafeBase64(verificationCode);
+                const answer = await fetch("/api/internal/v1/auth/email-verification/confirm", {
+                    method: "PATCH",
+                    body: JSON.stringify({ "verificationCode": unencodedVerificationCode })
+                })
+                const answerJson = await answer.json();
+                if (answerJson.status !== "success") {
+                    console.error(answerJson);
+                    return;
+                }
+            }
+        })()
+    }, [verificationCode])
+
+    return <>...</>
+}
+
+function decodeUrlSafeBase64(encoded) {
+    encoded = encoded.replaceAll(/-/g, '+').replaceAll(/_/g, '/');
+    return Buffer.from(encoded, 'base64').toString();
 }
