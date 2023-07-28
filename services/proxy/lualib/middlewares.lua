f08abc5cc0f8d45d46ca2d45485949b36a8f3259
adzaria <adzaria@protonmail.com>
2023-07-28 11:07:49 +0300

    added comments to lua's code in the gateway, to set_headers and control_access_token

--- services/proxy/lualib/middlewares.lua
+++ services/proxy/lualib/middlewares.lua
@@ -2,7 +2,14 @@
 
 local _M = {}
 
--- sets headers that must be passed to the upstream services
+-- This function sets several HTTP headers for the current request.
+--
+-- The 'X-Forwarded-For' header is set to the IP address of the client.
+-- The 'X-Forwarded-Proto' header is set to the scheme of the request (http or https).
+-- The 'X-Content-Type-Options' header is set to 'nosniff' to prevent the browser from doing MIME-type sniffing.
+-- The 'X-Frame-Options' header is set to 'DENY' to prevent clickjacking attacks.
+-- The 'X-XSS-Protection' header is set to '1; mode=block' to enable XSS filtering.
+-- The 'X-Request-Id' header is set to the unique ID of the request from NGINX.
 function _M.set_headers()
     ngx.req.set_header("X-Forwarded-For", ngx.var.remote_addr)
     ngx.req.set_header("X-Forwarded-Proto", ngx.var.scheme)
@@ -12,9 +19,10 @@ function _M.set_headers()
     ngx.req.set_header("X-Request-Id", ngx.var.request_id)
 end
 
--- checks if the request is authorized.
--- it only checks if the authorization header is valid
--- it is not resource-specific
+-- This function controls the existence and validity of the token provided in the authorization header.
+--
+-- It does not verify the token's permissions against a specific resource.
+-- The endpoint it calls must not expose sensitive data as the response is passed to the client.
 function _M.control_access_token()
     local res = ngx.location.capture("/api/internal/v1/auth/token/authorize", { method = ngx.HTTP_POST })
 
